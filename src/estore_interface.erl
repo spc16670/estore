7fd3ef9104afd209b2f75bdb1ea40550d93cf649
coolfeature <szymonpiotrczaja@gmail.com>
2015-08-13 15:49:45 +0100

    erl_interface temporarily relaxed

--- src/estore_interface.erl
+++ src/estore_interface.erl
@@ -1,25 +1,25 @@
 -module(estore_interface).
 
 -callback init(Adapter :: atom()) -> 
-  'ok'|tuple('error', Reason :: string()).
+  'ok'|{'error', Reason :: string()}.
 
 -callback models(Adapter :: atom()) -> 
-  Records :: list() | tuple('error', Reason :: string()).
+  Records :: list() | {'error', Reason :: string()}.
  
--callback new(Adapter :: atom(),Record :: record()) -> 
-  'ok'|tuple('error', Reason :: string()).
+-callback new(Adapter :: atom(),Record :: any()) -> 
+  'ok'|{'error', Reason :: string()}.
  
--callback save(Adapter :: atom(),Record :: record()) -> 
-  tuple('ok', Id :: integer())|tuple('error', Reason :: string()).
+-callback save(Adapter :: atom(),Record :: any()) -> 
+  {'ok', Id :: integer()}|{'error', Reason :: string()}.
 
--callback delete(Adapter :: atom(),Record :: record()) -> 
-  tuple('ok',Count :: integer())|tuple('error', Reason :: string()).
+-callback delete(Adapter :: atom(),Record :: any()) -> 
+  {'ok',Count :: integer()}|{'error', Reason :: string()}.
 
--callback delete(Adapter :: atom(),Record :: record(),Conditions :: list()) -> 
-  tuple('ok',Count :: integer())|tuple('error', Reason :: string()).
+-callback delete(Adapter :: atom(),Record :: any(),Conditions :: list()) -> 
+  {'ok',Count :: integer()}|{'error', Reason :: string()}.
 
 -callback find(Adapter :: atom(),Name :: atom(),Conditions :: list()) -> 
-  Records :: list() | tuple('error', Reason :: string()).
+  Records :: list() | {'error', Reason :: string()}.
 
 -callback find(Adapter :: atom(),Name :: atom(),Where :: list(), OrderBy :: list(), Limit :: integer(), Offset :: integer()) -> 
-  Records :: list() | tuple('error', Reason :: string()).
+  Records :: list() | {'error', Reason :: string()}.
